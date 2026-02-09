a7c783d7d6663816cd334f340ef2eb9384cf853f
psytzou <147252050+psytzou@users.noreply.github.com>
2026-02-10 03:42:52 +0800

    Add files via upload

--- scripts/iperf_final_def.py
+++ scripts/iperf_final_def.py
@@ -15,15 +15,10 @@ eth_n0 = "10.0.4.2"
 eth_n1 = "10.0.5.2"
 eth_n2 = "10.0.6.2"
 eth_n3 = "10.0.7.2"
-# node_n0 = "$n0"
-# node_n1 = "$n1"
-# node_n2 = "$n2"
-# node_n3 = "$n3"
 node_n0 = "10.0.4.3"
 node_n1 = "10.0.5.3"
 node_n2 = "10.0.6.3"
 node_n3 = "10.0.7.3"
-port = 5020
 
 file = ''
 
@@ -47,12 +42,12 @@ def pattern_check(protocol):
     subprocess.run(['tmux', 'send-keys', '-t', 'nd0', './idsreg reset', 'C-m'])
     time.sleep(1)  # wait to initialize
     subprocess.run(['tmux', 'send-keys', '-t', 'nd0', './idsreg pattern ABCDEFG', 'C-m'])
-    time.sleep(2)  # wait to initialize
+    time.sleep(1)  # wait to initialize
     subprocess.run(['tmux', 'send-keys', '-t', 'nd0', './idsreg allregs', 'C-m'])
-    for y in range(4):
-         subprocess.run(['tmux', 'send-keys', '-t', f'nd{y+1}', 'echo -n "ABCDEFG" > patternABCDEFG.txt' , 'C-m'])
-         subprocess.run(['tmux', 'send-keys', '-t', f'nd{y+1}', 'echo -n "aqrwetw" > pattern.txt', 'C-m'])
-    time.sleep(5)  # wait to initialize
+    subprocess.run(['tmux', 'send-keys', '-t', 'nd0', 'echo -n "ABCDEFG" > patternABCDEFG.txt' , 'C-m'])
+    time.sleep(1)  # wait to initialize
+    subprocess.run(['tmux', 'send-keys', '-t', 'nd0', 'echo -n "aqrwetwefreswre" > pattern.txt', 'C-m'])
+    time.sleep(1)  # wait to initialize
     print("Starting pattern check...")
     if protocol == 'udp':
         for m in range(1,5):   # nd1 -- node_0 -- nd0 in terminal        
@@ -61,12 +56,9 @@ def pattern_check(protocol):
                 if m == k+1:
                     pass
                 elif (m == k or m == k+4):
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', 'C-m'])
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -u -p {port} -t 30 -F patternABCDEFG.txt > /dev/null 2>&1', 'C-m'])
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -u -p 5005 -l 512 -t 30 -b 900M -F patternABCDEFG.txt > /dev/null 2>&1', 'C-m'])
                 else:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', 'C-m'])
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -u -p {port} -t 30 -F pattern.txt > /dev/null 2>&1', 'C-m'])
-                #time.sleep(5)
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -u -p 5005 -l 512 -t 30 -b 900M -F pattern.txt > /dev/null 2>&1', 'C-m'])
     else: #tcp
         for m in range(1,5):   # nd1 -- node_0 -- nd0 in terminal        
             for k in range(4):
@@ -74,13 +66,9 @@ def pattern_check(protocol):
                 if m == k+1:
                     pass
                 elif (m == k or m == k+4):
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', 'C-m'])
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -p {port} -F patternABCDEFG.txt > /dev/null 2>&1 &', 'C-m'])                    
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -p 5005 -t 30 -F patternABCDEFG.txt > /dev/null 2>&1', 'C-m'])
                 else:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', 'C-m'])
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -p {port} -F pattern.txt > /dev/null 2>&1 &', 'C-m'])
-                time.sleep(5)
-                    
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{m}', f'iperf -c {node_n} -p 5005 -t 30 -F pattern.txt > /dev/null 2>&1', 'C-m'])
 
   
 
@@ -93,30 +81,25 @@ def iperf_test(protocol='tcp',bitfile='nic'):
     subprocess.run(['tmux', 'send-keys', '-t', 'nd0', f'nf_download {B_MAP[bitfile]}', 'C-m'])
     time.sleep(10)
 
-    if bitfile == 'nic':
-        pass
-    else:
-        subprocess.run(['tmux', 'send-keys', '-t', 'nd0', 'rkd &', 'C-m'])
     if protocol == 'udp':
+        subprocess.run(['tmux', 'send-keys', '-t', 'nd0', 'rkd &', 'C-m'])
         time.sleep(1)
         print("Starting iperf server...")
         for i in range(4):
-            subprocess.run(['tmux', 'send-keys', '-t', f'nd{i+1}', f'{P_MAP[protocol]} -p {port} & ', 'C-m'])
+            subprocess.run(['tmux', 'send-keys', '-t', f'nd{i+1}', f'{P_MAP[protocol]} -p 5005 & ', 'C-m'])
         time.sleep(5)
 
         if bitfile == 'ids':
-            pattern_check('udp')
+            pattern_check(udp)
         else:
 
             for j in range(4):
                 eth_n = globals()[f'eth_n{j}']
                 node_n = globals()[f'node_n{j}']   #nd1 -- node_0 -- nd0 in terminal
                 if j == 3:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{1}', f'iperf -c {node_n} -u -p {port} -l 512 -t 30 -b 1000M > /dev/null 2>&1', 'C-m'])
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{1}', f'iperf -c {node_n} -u -p 5005 -l 512 -t 30 -b 900M > /dev/null 2>&1', 'C-m'])
                 else:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{j+2}', f'iperf -c {node_n} -u -p {port} -l 512 -t 30 -b 1000M > /dev/null 2>&1', 'C-m'])
-                    
- 
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{j+2}', f'iperf -c {node_n} -u -p 5005 -l 512 -t 30 -b 900M > /dev/null 2>&1', 'C-m'])
 
            
             # print(f"Node {j+1} testing against {eth_n}")
@@ -124,34 +107,31 @@ def iperf_test(protocol='tcp',bitfile='nic'):
     else: #tcp
         print("Starting iperf server...")
         for i in range(4):
-            subprocess.run(['tmux', 'send-keys', '-t', f'nd{i+1}', f'{P_MAP[protocol]} -p {port} & ', 'C-m'])
+            subprocess.run(['tmux', 'send-keys', '-t', f'nd{i+1}', f'{P_MAP[protocol]} -p 5005 & ', 'C-m'])
         time.sleep(5)
 
         if bitfile == 'ids':
-            pattern_check('tcp')
+            pattern_check(tcp)
         else:
             for j in range(4):
                 eth_n = globals()[f'eth_n{j}']
                 node_n = globals()[f'node_n{j}']
                 if j == 3:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{1}', f'iperf -c {node_n} -p {port} > /dev/null 2>&1', 'C-m'])
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{1}', f'iperf -c {node_n} -p 5005 > /dev/null 2>&1', 'C-m'])
                 else:
-                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{j+2}', f'iperf -c {node_n} -p {port} > /dev/null 2>&1', 'C-m'])
+                    subprocess.run(['tmux', 'send-keys', '-t', f'nd{j+2}', f'iperf -c {node_n} -p 5005 > /dev/null 2>&1', 'C-m'])
 
             
             # print(f"Node {j+1} testing against {eth_n}")
     
+    time.sleep(60)
     if bitfile == 'ids':
         subprocess.run(['tmux', 'send-keys', '-t', 'nd0', './idsreg matches', 'C-m'])
-        time.sleep(5)
         server_result = subprocess.check_output(['tmux', 'capture-pane', '-t', f'nd0', '-p','-S','-','-E','-']).decode('utf-8')
         logging(server_result)
-    time.sleep(30)
-    subprocess.run(['tmux', 'send-keys', '-t', f'nd0', f'killall rkd ', 'C-m'])
     for i in range(4):
         server_result = subprocess.check_output(['tmux', 'capture-pane', '-t', f'nd{i+1}', '-p','-S','-','-E','-']).decode('utf-8')
         logging(server_result)
-        time.sleep(1)
 
 
 def main():
